3ef953b4fc7ba2fa50b4aed35661a15e077a7f4d
VaishnaviSubramanian05 <vaishuvaish700@gmail.com>
2026-05-13 01:11:41 +0530

    Updated production URLs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 
-import Home from "./pages/Home";
-import Explore from "./pages/Explore";
+import Home from "./pages/home";
+import Explore from "./pages/explore";
 import StoryDetails from "./pages/StoryDetails";
 import CreateStory from "./pages/CreateStory";
 import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Signup from "./pages/Signup";
 
 function App() {
